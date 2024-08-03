5cd60151b5b8e50b17d8b85ea9db6ff60cdb0231
Markus <28785953+markusjx@users.noreply.github.com>
2024-08-03 21:02:56 +0200

    feat(libfreenect2-rs): add feature for catching frame listener panics
    Signed-off-by: Markus <28785953+MarkusJx@users.noreply.github.com>

--- libfreenect2-rs/src/types/frame_listener.rs
+++ libfreenect2-rs/src/types/frame_listener.rs
@@ -1,23 +1,38 @@
 use cxx::UniquePtr;
+#[cfg(feature = "catch-frame-listener-panic")]
 use std::panic::{catch_unwind, UnwindSafe};
 
 use crate::ffi;
+use crate::ffi::CallContext;
 use crate::types::frame::Frame;
 use crate::types::frame_type::FrameType;
 
 pub struct FrameListener<'a>(pub(crate) UniquePtr<ffi::libfreenect2::FrameListener<'a>>);
 
 impl<'a> FrameListener<'a> {
+  #[cfg(feature = "catch-frame-listener-panic")]
   pub fn new<F: Fn(FrameType, Frame) + UnwindSafe + Clone + 'a>(f: F) -> anyhow::Result<Self> {
     let ctx = Box::new(ffi::CallContext {
       func: Box::new(move |ty, frame| {
         let func = f.clone();
         if let Err(e) = catch_unwind(move || func(ty, frame)) {
-          eprintln!("Panic in frame listener: {:?}", e);
+          log::error!("Panic in frame listener: {:?}", e);
         }
       }),
     });
 
+    Self::create_self(ctx)
+  }
+
+  #[cfg(not(feature = "catch-frame-listener-panic"))]
+  pub fn new<F: Fn(FrameType, Frame) + 'a>(f: F) -> anyhow::Result<Self> {
+    #[cfg(not(feature = "catch-frame-listener-panic"))]
+    let ctx = Box::new(ffi::CallContext { func: Box::new(f) });
+
+    Self::create_self(ctx)
+  }
+
+  fn create_self(ctx: Box<CallContext>) -> anyhow::Result<Self> {
     ffi::libfreenect2::create_frame_listener(ctx, |frame_type, frame, ctx| {
       let ctx = ctx.as_ref();
       let func = ctx.func.as_ref();
