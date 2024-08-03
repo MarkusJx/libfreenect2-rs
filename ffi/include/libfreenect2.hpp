537bf8b1a3b9d129f7aeaeb42d755946c5b616c2
Markus <28785953+markusjx@users.noreply.github.com>
2024-08-03 13:29:34 +0200

    ci: add lint workflow
    Signed-off-by: Markus <28785953+MarkusJx@users.noreply.github.com>

--- ffi/include/libfreenect2.hpp
+++ ffi/include/libfreenect2.hpp
@@ -3,34 +3,37 @@
 
 #include <libfreenect2/libfreenect2.hpp>
 #include <memory>
+
 #include "freenect2_device.hpp"
-#include "rust/cxx.h"
 #include "macros.hpp"
+#include "rust/cxx.h"
 
 namespace libfreenect2_ffi {
-    class Freenect2 {
-    public:
-        Freenect2();
+  class Freenect2 {
+   public:
+    Freenect2();
 
-        ~Freenect2();
+    ~Freenect2();
 
-        LIBFREENECT2_RS_FUNC int32_t enumerate_devices();
+    LIBFREENECT2_RS_FUNC int32_t enumerate_devices();
 
-        LIBFREENECT2_RS_FUNC rust::String get_device_serial_number(int32_t idx);
+    LIBFREENECT2_RS_FUNC rust::String get_device_serial_number(int32_t idx);
 
-        LIBFREENECT2_RS_FUNC rust::String get_default_device_serial_number();
+    LIBFREENECT2_RS_FUNC rust::String get_default_device_serial_number();
 
-        LIBFREENECT2_RS_FUNC std::unique_ptr<Freenect2Device> open_device_by_id(int32_t idx);
+    LIBFREENECT2_RS_FUNC std::unique_ptr<Freenect2Device> open_device_by_id(
+        int32_t idx);
 
-        LIBFREENECT2_RS_FUNC std::unique_ptr<Freenect2Device> open_device_by_serial(rust::Str serial);
+    LIBFREENECT2_RS_FUNC std::unique_ptr<Freenect2Device> open_device_by_serial(
+        rust::Str serial);
 
-        LIBFREENECT2_RS_FUNC std::unique_ptr<Freenect2Device> open_default_device();
+    LIBFREENECT2_RS_FUNC std::unique_ptr<Freenect2Device> open_default_device();
 
-    private:
-        libfreenect2::Freenect2 freenect2;
-    };
+   private:
+    libfreenect2::Freenect2 freenect2;
+  };
 
-    LIBFREENECT2_RS_FUNC std::unique_ptr<Freenect2> create_freenect2();
-}
+  LIBFREENECT2_RS_FUNC std::unique_ptr<Freenect2> create_freenect2();
+}  // namespace libfreenect2_ffi
 
-#endif //FFI_LIBFREENECT2_HPP
+#endif  // FFI_LIBFREENECT2_HPP
